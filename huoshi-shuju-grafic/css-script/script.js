d4cdc356db14ee786a640d62c6bbf7eb3c1d0797
huoshi-taostar <wlexiang123@gmail.com>
2026-01-31 11:41:26 +0100

    Update script.js

--- huoshi-shuju-grafic/css-script/script.js
+++ huoshi-shuju-grafic/css-script/script.js
@@ -50,7 +50,7 @@
             { date: "2026-01-13", number: 3310 },
             { date: "2026-01-15", number: 3320 },       
             { date: "2026-01-26", number: 3310 },
-            { date: "2026-01-27", number: 2660 }
+            { date: "2026-01-27", number: 3660 }
         ];
 
         function updateStats() {
@@ -194,6 +194,7 @@ async function fetchEURCNY() {
 // 初始化并每小时更新一次
 fetchEURCNY();
 setInterval(fetchEURCNY, 3600000);
+
 
 
 
